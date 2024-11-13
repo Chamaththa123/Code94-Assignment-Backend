760812fda6c67df092e2a11ffb8f395d8915fd74
Chamaththa123 <shamodchamaththa@gmail.com>
2024-11-13 15:19:11 +0530

    create db connection

---
+++ Backend/db/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// Function to connect to MongoDB
+const connectDB = async () => {
+  try {
+    // Connecting to MongoDB using provided URI
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB"); // Logging success message
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error); // Logging error message
+  }
+};
+
+module.exports = connectDB;
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,13 @@
-const express = require("express");
+const express = require('express');
+const connectDB = require('./db/db');
+require('dotenv').config();
+
 const app = express();
-const PORT = 8001;
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connectDB();
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
